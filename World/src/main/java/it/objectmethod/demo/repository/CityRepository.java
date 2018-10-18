be7d48e5fc32b15fc1862c429c4a9c6648274557
Ivan Maltese <ivan.maltese@objectmethod.it>
2018-10-18 12:09:28 +0200

    test conflitto

--- World/src/main/java/it/objectmethod/demo/repository/CityRepository.java
+++ World/src/main/java/it/objectmethod/demo/repository/CityRepository.java
@@ -10,8 +10,8 @@ import it.objectmethod.demo.entity.City;
 @Repository
 public interface CityRepository extends JpaRepository<City, Long>{
 
+	//Voglio un conflitto
 	public List<City> findByNameStartingWith(String name);
 	public List<City> findCityByCodeOrderByNameAsc(String code);
-	public City findById(long id);
 	public List<City> findCityByCode(String code);
 }
